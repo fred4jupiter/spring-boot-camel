1b308bcd71b34e395a7c489dc9a2e57444366be8
Staehler, Michael <michael.staehler@opitz-consulting.com>
2019-02-11 11:05:39 +0100

    refactoring

--- src/main/java/de/fred4jupiter/spring/boot/camel/MyCamelRoutes.java
+++ src/main/java/de/fred4jupiter/spring/boot/camel/MyCamelRoutes.java
@@ -1,13 +1,17 @@
 package de.fred4jupiter.spring.boot.camel;
 
 import org.apache.camel.spring.SpringRouteBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyCamelRoutes extends SpringRouteBuilder {
 
+    @Autowired
+    private ContentEnricher contentEnricher;
+
     @Override
     public void configure() {
-        from("file:inbox").to("bean:contentEnricher").to("file:outbox", "mock:out");
+        from("file:inbox").bean(contentEnricher).to("file:outbox", "mock:out");
     }
 }
