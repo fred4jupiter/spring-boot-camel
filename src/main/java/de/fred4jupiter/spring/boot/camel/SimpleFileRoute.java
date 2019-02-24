5b6be70875b93a3440fb258e0670d85301923d4e
fred4jupiter <hamsterhase@gmx.de>
2019-02-24 11:39:19 +0100

    refactoring

--- src/main/java/de/fred4jupiter/spring/boot/camel/SimpleFileRoute.java
+++ src/main/java/de/fred4jupiter/spring/boot/camel/SimpleFileRoute.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class MyCamelRoutes extends SpringRouteBuilder {
+public class SimpleFileRoute extends SpringRouteBuilder {
 
     @Autowired
     private ContentEnricher contentEnricher;
