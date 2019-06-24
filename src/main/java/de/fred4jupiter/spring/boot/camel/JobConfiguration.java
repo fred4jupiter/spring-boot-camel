e19091c6d8858d63e0243aa7fc333c0b4a761b5b
Staehler, Michael <michael.staehler@opitz-consulting.com>
2019-06-24 16:18:44 +0200

    added scheduling example

---
+++ src/main/java/de/fred4jupiter/spring/boot/camel/JobConfiguration.java
@@ -0,0 +1,23 @@
+package de.fred4jupiter.spring.boot.camel;
+
+import de.fred4jupiter.spring.boot.camel.quartz.ClusterExecutionDemoJob;
+import org.quartz.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JobConfiguration {
+
+    @Bean
+    public JobDetail jobADetails() {
+        return JobBuilder.newJob(ClusterExecutionDemoJob.class).withIdentity("sampleJobA").storeDurably().build();
+    }
+
+    @Bean
+    public Trigger jobATrigger(JobDetail jobADetails) {
+        return TriggerBuilder.newTrigger().forJob(jobADetails)
+                .withIdentity("sampleTriggerA")
+                .withSchedule(CronScheduleBuilder.cronSchedule("0/2 * * ? * * *"))
+                .build();
+    }
+}
